075c6798eef2012787e0981675570d352bc3d845
smertch <pomazsergej@gmail.com>
2017-09-24 07:29:35 +0300

    form

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smertch
+ * Date: 23.09.17
+ * Time: 22:12
+ */
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ExampleController extends Controller
+{
+    public function getExample()
+    {
+        return view('example');
+    }
+
+    public function postExample(Request $request)
+    {
+        // validate the user-entered Captcha code when the form is submitted
+        $code = $request->input('CaptchaCode');
+        $isHuman = captcha_validate($code);
+
+        if ($isHuman) {
+            // TODO: Captcha validation passed, perform protected  action
+        } else {
+            // TODO: Captcha validation failed, show error message
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+https://prnt.sc/gkvg6e https://prnt.sc/gkvgdu
+After cloning the project, create (empty) and mount the database in a file (.env)
+and then on the command line, going to the directory where the project is located: 1 composer update 2 php artisan migrate
+
+Go to the browser and check!
+
+//////////////////////////////////////////////////////////////
+После клонирования проекта, создайте (пустую) и подключите базу данных в файле (.env)
+и далее в командной строке перейдя в директорию, где расположен проект выполнить: 1 composer update 2 php artisan migrate
+
+Переходите в браузер и проверяйте!
